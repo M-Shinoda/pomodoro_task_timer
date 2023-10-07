0caac283ee1743c3dbbf15558abb74d73652566c
M-Shinoda <sect011masami@gmail.com>
2023-10-07 16:50:14 +0900

    fix

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pomodoro_task_timer/freezed/task_state.dart';
 import 'package:pomodoro_task_timer/views/timer_view.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends ConsumerWidget {
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
       home: Scaffold(
-        body: TimerView(),
+        body: const TimerView(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ref.read(taskListProvider.notifier).addTask(TaskState.create(
+                title: "Task hoge", duration: const Duration(minutes: 50)));
+          },
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
